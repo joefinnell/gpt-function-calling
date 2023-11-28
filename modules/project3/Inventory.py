16e7e78bee798dfcd01b149e3c076c378104abb0
Joe Finnell <joefinnell24@gmail.com>
2023-11-27 21:26:36 -0800

    create inventory module

---
+++ modules/project3/Inventory.py
@@ -0,0 +1,27 @@
+class Inventory:
+    def __init__(self):
+        self.inventory = {}
+
+    def add_bike(self, brand, model, quantity):
+        if brand not in self.inventory:
+            self.inventory[brand] = {}
+        if model not in self.inventory[brand]:
+            self.inventory[brand][model] = quantity
+        else:
+            self.inventory[brand][model] += quantity
+
+    def purchase_bike(self, brand, model):
+        if brand in self.inventory and model in self.inventory[brand] and self.inventory[brand][model] > 0:
+            self.inventory[brand][model] -= 1
+            return True
+        return False
+
+    def get_available_quantity(self, brand, model):
+        if brand in self.inventory and model in self.inventory[brand]:
+            return self.inventory[brand][model]
+        return 0
+
+    def get_available_models(self, brand):
+        if brand in self.inventory:
+            return list(self.inventory[brand].keys())
+        return []
